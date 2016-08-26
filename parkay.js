aea5a37b87c1b739cb4b77dcb058cb96fdf609e7
Philip Del Vecchio <pdv@gc.com>
2016-08-26 13:05:52 -0400

    sliderize

--- parkay.js
+++ parkay.js
@@ -1,16 +1,17 @@
 'use strict'
 
 let canvas = document.getElementById('canvas')
+let slider = document.getElementById('slider')
 let ctx = canvas.getContext('2d')
 const cw = canvas.width
 const ch = canvas.height
 const wh = 20
-
+const edges = createEdges()
 
 /**
  * Returns a list of the edges in each generation
  */
-function createGrid() {
+function createEdges() {
     let edges = []
     const gw = cw / wh
     const gh = ch / wh
@@ -33,16 +34,35 @@ function createGrid() {
     return edges
 }
 
-function drawGrid(edges) {
-    for (let i = 0; i < edges.length; i++) {
-        for (let j = 0; j < edges[i].length; j++) {
-            let [p1, p2] = edges[i][j]
-            ctx.beginPath();
-            ctx.moveTo(p1.x, p1.y)
-            ctx.lineTo(p2.x, p2.y)
-            ctx.stroke()
-        }
+
+function offsetMidpoint(p1, p2, offset) {
+    let isVertical = p1.x == p2.x
+    return {
+        x: (p1.x + p2.x) / 2 + (isVertical * offset),
+        y: (p1.y + p2.y) / 2 + (!isVertical * offset)
     }
 }
 
-drawGrid(createGrid())
+
+function drawEdge(p1, p2, t) {
+    const mid = offsetMidpoint(p1, p2, 0)
+    const cp1 = offsetMidpoint(p1, mid, t * slider.value)
+    const cp2 = offsetMidpoint(mid, p2, -1 * t * slider.value)
+    ctx.beginPath()
+    ctx.moveTo(p1.x, p1.y)
+    ctx.lineTo(cp1.x, cp1.y)
+    ctx.lineTo(cp2.x, cp2.y)
+    ctx.lineTo(p2.x, p2.y)
+    ctx.stroke()
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, cw, ch)
+    for (let t = 0; t < edges.length; t++) {
+        edges[t].map(([p1, p2]) => drawEdge(p1, p2, t))
+    }
+    window.requestAnimationFrame(draw)
+}
+
+draw()
